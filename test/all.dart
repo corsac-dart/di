3b36addd81f938eb7997a4044a9ef416c2d39877
Anatoly Pulyaevskiy <anatoly@sparkcentral.com>
2015-12-12 17:11:42 -0800

    Grouped container tests; added travis config

---
+++ test/all.dart
@@ -0,0 +1,7 @@
+library corsac_di.test.all;
+
+import 'container_test.dart' as container_test;
+
+void main() {
+  container_test.main();
+}
--- test/container_test.dart
+++ test/container_test.dart
@@ -5,64 +5,66 @@ import 'package:test/test.dart';
 import 'package:corsac_di/di.dart' as di;
 
 void main() {
-  test("it resolves objects automagically", () {
-    var container = new di.Container();
-    var service = container.get(MyService);
-    expect(service, new isInstanceOf<MyService>());
-    var secondCallService = container.get(MyService);
-    expect(secondCallService, same(service));
-  });
-  test("it resolves objects", () {
-    var definitions = {MyService: di.object(),};
+  group('Container:', () {
+    test("it resolves objects automagically", () {
+      var container = new di.Container();
+      var service = container.get(MyService);
+      expect(service, new isInstanceOf<MyService>());
+      var secondCallService = container.get(MyService);
+      expect(secondCallService, same(service));
+    });
+    test("it resolves objects", () {
+      var definitions = {MyService: di.object(),};
 
-    var container = new di.Container.build(definitions);
-    var service = container.get(MyService);
-    expect(service, new isInstanceOf<MyService>());
-    var secondCallService = container.get(MyService);
-    expect(secondCallService, same(service));
-  });
+      var container = new di.Container.build(definitions);
+      var service = container.get(MyService);
+      expect(service, new isInstanceOf<MyService>());
+      var secondCallService = container.get(MyService);
+      expect(secondCallService, same(service));
+    });
 
-  test("it resolves objects with custom constructor", () {
-    var definitions = {
-      MyServiceWithNamedConstructor: di.object()..constructor = 'getNew',
-    };
+    test("it resolves objects with custom constructor", () {
+      var definitions = {
+        MyServiceWithNamedConstructor: di.object()..constructor = 'getNew',
+      };
 
-    var container = new di.Container.build(definitions);
-    var service = container.get(MyServiceWithNamedConstructor);
-    expect(service, new isInstanceOf<MyServiceWithNamedConstructor>());
-    var secondCallService = container.get(MyServiceWithNamedConstructor);
-    expect(secondCallService, same(service));
-  });
+      var container = new di.Container.build(definitions);
+      var service = container.get(MyServiceWithNamedConstructor);
+      expect(service, new isInstanceOf<MyServiceWithNamedConstructor>());
+      var secondCallService = container.get(MyServiceWithNamedConstructor);
+      expect(secondCallService, same(service));
+    });
 
-  test("it resolves objects with positional parameters", () {
-    var definitions = {
-      OneParameter: di.object()
-        ..bindParameter('prop', 1)
-        ..bindParameter('foo', 'example'),
-    };
+    test("it resolves objects with positional parameters", () {
+      var definitions = {
+        OneParameter: di.object()
+          ..bindParameter('prop', 1)
+          ..bindParameter('foo', 'example'),
+      };
 
-    var container = new di.Container.build(definitions);
-    OneParameter service = container.get(OneParameter);
-    expect(service, new isInstanceOf<OneParameter>());
-    expect(service.prop, equals(1));
-    expect(service.foo, equals('example'));
-  });
+      var container = new di.Container.build(definitions);
+      OneParameter service = container.get(OneParameter);
+      expect(service, new isInstanceOf<OneParameter>());
+      expect(service.prop, equals(1));
+      expect(service.foo, equals('example'));
+    });
 
-  test("it fails to resolve if positional parameter is not bound", () {
-    var definitions = {
-      OneParameter: di.object()..bindParameter('foo', 'example'),
-    };
+    test("it fails to resolve if positional parameter is not bound", () {
+      var definitions = {
+        OneParameter: di.object()..bindParameter('foo', 'example'),
+      };
 
-    var container = new di.Container.build(definitions);
-    expect(() => container.get(OneParameter), throwsStateError);
-  });
+      var container = new di.Container.build(definitions);
+      expect(() => container.get(OneParameter), throwsStateError);
+    });
 
-  test("it resolves environment variables", () {
-    var definitions = {'CurrentDir': di.env('PWD')};
-    var container = new di.Container.build(definitions);
-    var result = container.get('CurrentDir');
-    expect(result, isNotNull);
-    expect(result, equals(Platform.environment['PWD']));
+    test("it resolves environment variables", () {
+      var definitions = {'CurrentDir': di.env('PWD')};
+      var container = new di.Container.build(definitions);
+      var result = container.get('CurrentDir');
+      expect(result, isNotNull);
+      expect(result, equals(Platform.environment['PWD']));
+    });
   });
 }
 
